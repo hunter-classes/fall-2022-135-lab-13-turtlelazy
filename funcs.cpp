68a53722738336d817b83cdda96b63646d26e584
turtlelazy <ishraqmahid@gmail.com>
2022-12-08 20:49:59 -0500

    task A

--- funcs.cpp
+++ funcs.cpp
@@ -1,1 +1,11 @@
 #include "funcs.h"
+#include <string>
+
+std::string printRange(int left, int right){
+    if (left == right){
+        return std::to_string(left);
+    }
+    
+    return std::to_string(left) + " " + printRange(left+1,right);
+}
+
--- main.cpp
+++ main.cpp
@@ -1,2 +1,7 @@
 #include <iostream>
 #include "funcs.h"
+
+int main(){
+    std::cout << printRange(-2, 10) << "\n";
+    return 0;
+}
